6daec2622f4c4ca577199328be7ac76d8510b1a9
mizumasa <masaru0mizuochi@gmail.com>
2019-07-12 00:51:48 +0900

    toio module first commit

---
+++ toio_message.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#toio BLE sample (with Adafruit_BluefruitLE)
+#auther: mizumasa
+
+import binascii
+from toio_config import *
+
+################
+#### MOTOR #####
+################
+
+def write_data_motor(l,r):
+    assert -100 <= l and l<= 100
+    assert -100 <= r and r<= 100
+    return "0101{:02x}{:02x}02{:02x}{:02x}".format( (l<0)+1, abs(l), (r<0)+1, abs(r) )
+
+def write_data_motor_timer(l,r,t):
+    #l = -100 ~ 100
+    #r = -100 ~ 100
+    #t = 0 ~ 2550
+    # time to stop (t ms)
+    assert -100 <= l and l<= 100
+    assert -100 <= r and r<= 100
+    assert 0 <= t and t<= 2550
+    return "0201{:02x}{:02x}02{:02x}{:02x}{:02x}".format( (l<0)+1, abs(l), (r<0)+1, abs(r), int(t/10) )
+
+################
+#### LIGHT #####
+################
+
+def write_data_light(t,r,g,b):
+    assert 0 <= t and t<= 2550
+    assert 0 <= r and r<= 255
+    assert 0 <= g and g<= 255
+    assert 0 <= b and b<= 255
+    return "03{:02x}0101{:02x}{:02x}{:02x}".format( int(t/10), r, g, b )
+
+def write_data_light_seq(n,t1,r1,g1,b1,t2,r2,g2,b2):
+    assert 0 <= n and n<= 255
+    assert 0 <= t1 and t1<= 2550
+    assert 0 <= r1 and r1<= 255
+    assert 0 <= g1 and g1<= 255
+    assert 0 <= b1 and b1<= 255
+    assert 0 <= t2 and t2<= 2550
+    assert 0 <= r2 and r2<= 255
+    assert 0 <= g2 and g2<= 255
+    assert 0 <= b2 and b2<= 255
+    return "04{:02x}02{:02x}0101{:02x}{:02x}{:02x}{:02x}0101{:02x}{:02x}{:02x}".format( 
+        n, int(t1/10), r1, g1, b1, int(t2/10), r2, g2, b2)
+
+def write_data_light_off():
+    return "01"
+
+################
+#### SOUND #####
+################
+
+def write_data_sound(sid,vol):
+    assert 0 <= sid and sid<= 10
+    assert 0 <= vol and vol<= 255
+    return "02{:02x}{:02x}".format( sid, vol )
+
+################
+####   ID   ####
+################
+
+def get_data_id(data):
+    l = hexD2list(data[2:])
+    return {"cx":l[0],"cy":l[1],"cr":l[2],"sx":l[3],"sy":l[4],"sr":l[5]}
+
+################
+#### SENSOR ####
+################
+
+def get_data_sensor(data):
+    l = hex2list(data)
+    return {"slope":l[1],"collision":l[2]}
+
+################
+#### BUTTON ####
+################
+
+def get_data_button(data):
+    l = hex2list(data)
+    return {"button":l[1]}
+
+################
+#### BATTERY ###
+################
+
+def get_data_battery(data):
+    l = hex2list(data)
+    return {"battery":l[0]}
+
+
+def hex2list(data):
+    assert len(data) % 2 == 0
+    out = []
+    for i in range(0,len(data),2):
+        out.append(int(data[i:i+2],16))
+    return out
+
+def hexD2list(data):
+    assert len(data) % 4 == 0
+    out = []
+    for i in range(0,len(data),4):
+        out.append(int(data[i:i+4],16))
+    return out
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
+
+
